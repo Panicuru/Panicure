391d34c1b10bbc1ed702bf9ef791b19c228ea0ab
Sam Meech-Ward <sam@meech-ward.com>
2016-03-05 15:17:29 -0800

    Call the completion block in the panic now method after 1.5 seconds to simulate a network request.

--- Panicure/PanicHelper.swift
+++ Panicure/PanicHelper.swift
@@ -12,6 +12,10 @@ import Foundation
 class PanicHelper: NSObject {
 
     static func startPanicingWithCompletion(completion: ((error: NSError?) -> Void)?) {
-    
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1.5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+            completion?(error: nil)
+        }
+        
+        
     }
 }
