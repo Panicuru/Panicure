def6671129b6569b5b80e3656131669cc5cf8b37
Vahan Harutyunyan <vahan@twotalltotems.com>
2016-03-05 11:54:11 -0800

    Added Panicure Extension

---
+++ PanicureExtension/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  PanicureExtension
+//
+//  Created by Vahan Harutyunyan on 2016-03-05.
+//  Copyright © 2016 Panicuru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
