45d7e6d610054d1783c4ec5f408405591a0dc69f
Sam Meech-Ward <sam@meech-ward.com>
2016-03-06 13:42:16 -0800

    When the success view controller loads, start capturing images

--- Panicure/Controllers/Success/SuccessViewController.swift
+++ Panicure/Controllers/Success/SuccessViewController.swift
@@ -12,16 +12,41 @@ class SuccessViewController: UIViewController {
 
     var panic: Panic?
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         startRecording()
     }
     
     private func startRecording() {
+        self.panic = EVANetworkHelper.sharedInstance().lastPanic as? Panic
         startRecordingImages()
     }
     
+    
+    let frontImageHelper = ImageCaptureHelper()
+    let backImageHelper = ImageCaptureHelper()
     private func startRecordingImages() {
+        frontImageHelper.frontCamera = true
+        backImageHelper.frontCamera = false
+        
+        let group = dispatch_group_create()
+        dispatch_group_enter(group)
+        dispatch_group_notify(group, dispatch_get_main_queue()) { () -> Void in
+            self.backImageHelper.capture { (image, error) -> Void in
+                if let image = image {
+                    EVANetworkHelper.sharedInstance().saveFrontCameraImage(image, toPanic: self.panic!)
+                }
+            }
+        }
+        
+        frontImageHelper.capture { (image, error) -> Void in
+            if let image = image {
+                EVANetworkHelper.sharedInstance().saveFrontCameraImage(image, toPanic: self.panic!)
+            }
+            dispatch_group_leave(group)
+        }
+        
         
     }
 }
